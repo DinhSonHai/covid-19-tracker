eaae21e8c5e03410fa283d06cd9c245bd10cf08b
DinhSonHai <sdinhhai@gmail.com>
2020-10-04 15:24:40 +0700

    Show data in card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
@@ -7,9 +7,17 @@ import styles from './App.module.css';
 import { fetchData } from './api/index';
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchData();
+      setData(data);
+    };
+    getData();
+  }, []);
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <CountryPicker />
       <Chart />
     </div>
--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,16 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
   try {
-    const res = await axios.get(url);
-    return res;
-  } catch (err) {}
+    const { data } = await axios.get(url);
+    const modifiedData = {
+      confirmed: data.confirmed,
+      recovered: data.recovered,
+      deaths: data.deaths,
+      lastUpdate: data.lastUpdate,
+    };
+    console.log(modifiedData);
+    return modifiedData;
+  } catch (err) {
+    console.log(err.response);
+  }
 };
